425d92b187dfea241b7642ff5b90d1c71ae6e7cd
Gabriel Todesco <gabriel.todesco@mouts.info>
2021-03-04 20:37:18 -0300

    rename componente card

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    {{ pokemon }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PokemonCard',
+  props: ['pokemon'],
+}
+</script>
+
+<style scoped>
+.card {
+  flex: 0 0 20em;
+  height: 20em;
+  margin: 1em;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border: 0.1em dashed #000;
+}
+</style>
