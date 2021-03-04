225673f6e5634864a808a53f3e8dc440784178f9
Gabriel Todesco <gabriel.todesco@mouts.info>
2021-03-04 20:37:45 -0300

    componente loader

---
+++ src/components/Loader.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class='loader' v-show="show" />
+</template>
+
+<script>
+export default {
+  name: 'Loader',
+  props: ['show'],
+}
+</script>
+
+<style>
+.loader {
+  border: 16px solid #ff0000;
+  border-radius: 50%;
+  border-top: 16px solid #000000;
+  border-left-color: #ffffff;
+  border-right-color: #ffffff;
+  width: 120px;
+  height: 120px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+</style>
