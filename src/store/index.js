98c37ba3c0a47688fb86d79e764a70bccfd7416c
Gabriel Todesco <gabriel.todesco@mouts.info>
2021-03-04 02:10:56 -0300

    vuex para buscar os dados dos cards

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,36 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import PokemonService from '../services/PokemonService'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
   state: {
+    cards: [],
+    cardSelected: null
   },
+
+  getters: {
+    cards(state) {
+      return state.cards;
+    }
+  },
+
   mutations: {
+    setCards(state, cards) {
+      state.cards = cards;
+    }
   },
+
   actions: {
+    async searchTasks(context, name = '') {
+
+      let response = await PokemonService.getByName(name);
+
+      context.commit('setCards', response.data);
+
+    }
   },
-  modules: {
-  }
+
 })
